b5cc39c23e9fd162cf0429166778791e42d472b0
Sven Lüchow <sven.luechow@icloud.com>
2022-03-18 23:46:45 +0100

    Add Time Icon

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,23 +1,46 @@
 import styled from 'styled-components';
 
+import { VscAccount } from 'react-icons/vsc';
+import { GoClock } from 'react-icons/go';
+
 export default function Entry({ text, author, color }) {
   return (
     <Card color={color}>
-      {text}
+      <TimeLogo>
+        <GoClock />
+      </TimeLogo>
+      <AccountLogo color={color}>
+        <VscAccount />
+      </AccountLogo>
       <Author color={color}>{author}</Author>
+      <p>{text}</p>
     </Card>
   );
 }
 
 const Card = styled.section`
-  display: grid;
+  display: flex;
   align-content: space-between;
   padding: 20px;
   max-width: 400px;
   border: 1px solid #ddd;
   height: 100%;
-  box-shadow: ${({ color }) => (color ? color : '#999')} 0px 13px 27px -5px,
+  box-shadow: ${({ color }) => (color ? color : '#999')} 0px 8px 20px -5px,
     ${({ color }) => (color ? color : '#999')} 0px 8px 16px -8px;
+  position: relative;
+`;
+
+const TimeLogo = styled.div`
+  color: ${({ color }) => (color ? color : '#999')};
+  position: absolute;
+  top: 10px;
+  right: 30px;
+`;
+
+const AccountLogo = styled.div`
+  color: ${({ color }) => (color ? color : '#999')};
+  position: absolute;
+  top: 10px;
 `;
 
 const Author = styled.p`
@@ -25,4 +48,7 @@ const Author = styled.p`
   text-transform: capitalize;
   font-size: 1rem;
   color: ${({ color }) => (color ? color : '#999')};
+  position: absolute;
+  top: 10px;
+  left: 45px;
 `;
